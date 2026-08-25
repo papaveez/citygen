c2301817b0dd71317fb9eee292cb63cc01f2de26
Vas <60276715+papaveez@users.noreply.github.com>
2026-08-25 17:09:24 +0300

    Refactored util into header/source

--- src/util.cpp
+++ src/util.cpp
@@ -38,3 +38,130 @@ perpendicular_distance(const Vector2& p, const Vector2& x0, const Vector2& x1) {
     return
         std::abs(d.y * p.x - d.x * p.y + x1.x * x0.y - x1.y * x0.x) / std::sqrt(l2);
 }
+
+
+Box::Box(Vector2 _min, Vector2 _max) :
+    min(_min),
+    max(_max)
+{}
+
+bool Box::is_empty() const {
+    return min.x >= max.x
+        || min.y >= max.y;
+}
+
+
+bool Box::contains(const Vector2& vec) const {
+    return 
+        min.x <= vec.x 
+        && vec.x < max.x
+        && min.y <= vec.y
+        && vec.y < max.y;
+}
+
+float Box::width() const {
+    return max.x - min.x;
+}
+
+float Box::height() const {
+    return max.y - min.y;
+}
+
+
+Vector2 Box::dimensions() const {
+    return max - min;
+}
+
+std::array<Box, 4> // TL TR BL BR
+Box::quadrants() const {
+    Vector2 mid = middle(min, max);
+
+    return {
+        Box(min, mid), 
+        Box({mid.x, min.y}, {max.x, mid.y}), 
+        Box({min.x, mid.y}, {mid.x, max.y}), 
+        Box(mid, max)
+    };
+}
+
+
+Box Box::get_quadrant(Quadrant q) {
+    Vector2 mid = middle(min, max);
+    switch (q) {
+        case TopLeft:
+            return Box(min, mid);
+        case TopRight:
+            return Box({mid.x, min.y}, {max.x, mid.y});
+        case BottomLeft:
+            return Box({min.x, mid.y}, {mid.x, max.y});
+        case BottomRight:
+            return Box(mid, max);
+    }
+}
+
+Quadrant Box::which_quadrant(Vector2 pos) {
+    Vector2 mid = middle(min, max);
+
+    for (Quadrant q : {TopLeft, TopRight, BottomLeft, BottomRight}) {
+        if (get_quadrant(q).contains(pos)) return q;
+    }
+}
+
+bool Box::operator==(const Box& other) const {
+    return other.min == min && other.max == max;
+}
+
+// box union.
+Box& Box::operator|=(const Box& other) {
+    min = {
+        std::min(min.x, other.min.x),
+        std::min(min.y, other.min.y)
+    };
+    max = {
+        std::max(max.x, other.max.x),
+        std::max(max.y, other.max.y)
+    };
+
+    return *this;
+}
+
+Box Box::operator|(const Box& other) const {
+    Box out = *this;
+    out |= other;
+    return out;
+}
+
+// box union with vector
+Box& Box::operator|=(const Vector2& other) {
+    Box boxed = Box(other, other);
+    *this |= boxed;
+    return *this;
+}
+
+Box Box::operator|(const Vector2& other) const {
+    Box out = *this;
+    out |= other;
+    return out;
+}
+
+
+// box intersection
+Box& Box::operator&=(const Box& other) {
+    min = {
+        std::max(min.x, other.min.x),
+        std::max(min.y, other.min.y)
+    };
+    max = {
+        std::min(max.x, other.max.x),
+        std::min(max.y, other.max.y)
+    };
+
+    return *this;
+}
+
+Box Box::operator&(const Box& other) const {
+    Box out = *this;
+    out &= other;
+    return out;
+}
+
--- src/util.h
+++ src/util.h
@@ -7,19 +7,14 @@
 #include "raylib.h"
 #include "raymath.h"
 
-
+// vector util functions
 std::ostream& operator<<(std::ostream& os, const Vector2& v); 
 
-float dot_product(const Vector2& a, const Vector2& b);
-
-Vector2 middle(Vector2 const& p1, Vector2 const& p2);
-
-
-float vector_angle(const Vector2& a, const Vector2& b);
-
-
-float 
-perpendicular_distance(const Vector2& p, const Vector2& x0, const Vector2& x1);
+float   dot_product           (const Vector2& a, const Vector2& b);
+Vector2 middle                (Vector2 const& p1, Vector2 const& p2);
+float   vector_angle          (const Vector2& a, const Vector2& b);
+float   perpendicular_distance(const Vector2& p, 
+                               const Vector2& x0, const Vector2& x1);
 
 
 enum Quadrant {
@@ -30,7 +25,6 @@ enum Quadrant {
 };
 
 
-
 struct Box {
     static constexpr float inf = std::numeric_limits<float>::infinity();
 
@@ -38,130 +32,36 @@ struct Box {
     Vector2 max{-inf, -inf};
 
     Box() = default;
-    Box(Vector2 _min, Vector2 _max) :
-        min(_min),
-        max(_max)
-    {}
+    Box(Vector2 _min, Vector2 _max);
 
-    bool is_empty() const {
-        return min.x >= max.x
-            || min.y >= max.y;
-    }
+    bool is_empty() const;
 
+    bool contains(const Vector2& vec) const;
 
-    bool contains(const Vector2& vec) const {
-        return 
-            min.x <= vec.x 
-            && vec.x < max.x
-            && min.y <= vec.y
-            && vec.y < max.y;
-    }
+    float width() const;
 
-    float width() const {
-        return max.x - min.x;
-    }
+    float height() const;
 
-    float height() const {
-        return max.y - min.y;
-    }
+    Vector2 dimensions() const;
 
+    // Given in TL, TR, BL, BR order
+    std::array<Box, 4> quadrants() const;
+    Box get_quadrant(Quadrant q);
+    Quadrant which_quadrant(Vector2 pos);
 
-    Vector2 dimensions() const {
-        return max - min;
-    }
-
-    std::array<Box, 4> // TL TR BL BR
-    quadrants() const {
-        Vector2 mid = middle(min, max);
-
-        return {
-            Box(min, mid), 
-            Box({mid.x, min.y}, {max.x, mid.y}), 
-            Box({min.x, mid.y}, {mid.x, max.y}), 
-            Box(mid, max)
-        };
-    }
-
-
-    Box get_quadrant(Quadrant q) {
-        Vector2 mid = middle(min, max);
-        switch (q) {
-            case TopLeft:
-                return Box(min, mid);
-            case TopRight:
-                return Box({mid.x, min.y}, {max.x, mid.y});
-            case BottomLeft:
-                return Box({min.x, mid.y}, {mid.x, max.y});
-            case BottomRight:
-                return Box(mid, max);
-        }
-    }
-
-    Quadrant which_quadrant(Vector2 pos) {
-        Vector2 mid = middle(min, max);
-
-        for (Quadrant q : {TopLeft, TopRight, BottomLeft, BottomRight}) {
-            if (get_quadrant(q).contains(pos)) return q;
-        }
-    }
-
-    bool operator==(const Box& other) const {
-        return other.min == min && other.max == max;
-    }
+    bool operator==(const Box& other) const;
 
     // box union.
-    Box& operator|=(const Box& other) {
-        min = {
-            std::min(min.x, other.min.x),
-            std::min(min.y, other.min.y)
-        };
-        max = {
-            std::max(max.x, other.max.x),
-            std::max(max.y, other.max.y)
-        };
-
-        return *this;
-    }
-
-    Box operator|(const Box& other) const {
-        Box out = *this;
-        out |= other;
-        return out;
-    }
+    Box& operator|=(const Box& other);
+    Box operator|(const Box& other) const;
 
     // box union with vector
-    Box& operator|=(const Vector2& other) {
-        Box boxed = Box(other, other);
-        *this |= boxed;
-        return *this;
-    }
-
-    Box operator|(const Vector2& other) const {
-        Box out = *this;
-        out |= other;
-        return out;
-    }
-
+    Box& operator|=(const Vector2& other);
+    Box operator|(const Vector2& other) const;
 
     // box intersection
-    Box& operator&=(const Box& other) {
-        min = {
-            std::max(min.x, other.min.x),
-            std::max(min.y, other.min.y)
-        };
-        max = {
-            std::min(max.x, other.max.x),
-            std::min(max.y, other.max.y)
-        };
-
-        return *this;
-    }
-
-    Box operator&(const Box& other) const {
-        Box out = *this;
-        out &= other;
-        return out;
-    }
+    Box& operator&=(const Box& other);
+    Box operator&(const Box& other) const;
 };
 
 // equivelant to foldr (|) boxes or foldr (|) vectors
